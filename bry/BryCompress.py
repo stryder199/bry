e6e2e7629494e8938667ccbebed7dd259263df0f
Mark Lessard <lessardme@hotmail.com>
2013-10-16 18:31:10 -0700

    blash

---
+++ bry/BryCompress.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 16, 2013
+
+@author: Mark
+'''
+class BryCompress:
+    
+    def compress(self, inputfilepath, outputfilepath):
+        pass
+    
+    def decompress(self, inputfilepath, outputfilepath):
+        pass
---
+++ bry/main.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 16, 2013
+
+@author: Mark
+'''
+from BryCompress import BryCompress
+
+bry = BryCompress()
+
+bry.compress("dic.txt", "bry.txt")
+
+#bry.decompress("bry.txt", "brydic.txt")
